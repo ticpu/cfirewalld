b9af6593a6351a30ee3caa5e360b23f0c1400d10
jerome.poulin <jerome.poulin@cauca.ca>
2016-12-07 18:53:10 -0500

    Prevent DoS by limiting log_drop/log_reject to 1 entry per second.

--- firewall.d/12_drops.sh
+++ firewall.d/12_drops.sh
@@ -7,11 +7,11 @@ fw_rule filter +reject any any -j REJECT
 fw_rule filter +reject any any -j DROP
 
 # Log Drop
-fw_rule filter +log_drop any any -j LOG --log-prefix "LD: "
+fw_rule filter +log_drop any any -m limit --limit 1/sec -j LOG --log-prefix "LD: "
 fw_rule filter +log_drop any any -j DROP
 
 # Log Reject
-fw_rule filter +log_reject any any -j LOG --log-prefix "LR: "
+fw_rule filter +log_reject any any -m limit --limit 1/sec -j LOG --log-prefix "LR: "
 fw_rule filter +log_reject any any -j +reject
 
 ## </Rules>
